5c7eac47f36d44dfe2abc8e580c63b052e45912d
bensawadogo <sawadogobenrachid0@gmail.com>
2026-09-13 04:12:48 +0000

    chore: enable shared Supabase gallery

--- js/supabase-config.js
+++ js/supabase-config.js
@@ -2,6 +2,6 @@
    Utilisez uniquement la clé anon/publishable, jamais un token sbp_ ou service_role. */
 window.SUPABASE_CONFIG = {
     url: 'https://faaurhhdpvyuxtodviqs.supabase.co',
-    anonKey: '',
+    anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZhYXVyaGhkcHZ5dXh0b2R2aXFzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzY3MzkzMjMsImV4cCI6MjA5MjMxNTMyM30.iNoiInLJyaQsxDWh2J3ikDW9m4hmdcjcJvDnKl9pWS8',
     bucket: 'wedding-photos'
 };
